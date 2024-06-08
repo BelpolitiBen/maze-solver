e1b23a4b233d9c47190db979b522ea92a13f7132
benbel <belpolitibenja@gmail.com>
2024-06-07 21:53:30 -0300

    added tests

---
+++ src/tests.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import MagicMock
+from maze import Maze
+from cell import Cell
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        
+    def test_maze_create_cells_2(self):
+        num_cols = 24
+        num_rows = 4
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    def test_maze_create_cells_3(self):
+        num_cols = 1
+        num_rows = 2
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        
+if __name__ == "__main__":
+    unittest.main()
